01727e91704635c417a9e4a52995b070d0ccc866
Linda Andersson <linda.29.andersson@gmail.com>
2022-08-22 08:52:32 +0200

    Setup for buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import react from 'react'
-
+import react from "react";
+import { Container, Button } from "react-bootstrap";
 
 function App() {
-
   return (
-    <div className="App">
-      
-    </div>
-  )
+    <Container>
+      <Button id="equals">=</Button>
+      <Button id="zero">0</Button>
+      <Button id="one">1</Button>
+      <Button id="two">2</Button>
+      <Button id="three">3</Button>
+      <Button id="four">4</Button>
+      <Button id="five">5</Button>
+      <Button id="six">6</Button>
+      <Button id="seven">7</Button>
+      <Button id="eight">8</Button>
+      <Button id="nine">9</Button>
+      <Button id="add">+</Button>
+      <Button id="subtract">-</Button>
+      <Button id="multiply">*</Button>
+      <Button id="divide">/</Button>
+      <Button id="decimal">.</Button>
+      <Button id="clear">AC</Button>
+      <Container id="display">Display calc here</Container>
+    </Container>
+  );
 }
 
-export default App
+export default App;
