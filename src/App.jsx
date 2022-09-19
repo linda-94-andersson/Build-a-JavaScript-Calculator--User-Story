03f158e5a5d54a16c510aa943fed7fcca9fcc487
Linda Andersson <linda.29.andersson@gmail.com>
2022-09-19 14:34:05 +0200

    Fixed Boostrap vissabilty and changes in the design

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,186 @@
 import react from "react";
-import { Container, Button } from "react-bootstrap";
+import { Container, Button, Col, Row } from "react-bootstrap";
 
 function App() {
   return (
     <Container>
-      <Button id="equals">=</Button>
-      <Button id="zero">0</Button>
-      <Button id="one">1</Button>
-      <Button id="two">2</Button>
-      <Button id="three">3</Button>
-      <Button id="four">4</Button>
-      <Button id="five">5</Button>
-      <Button id="six">6</Button>
-      <Button id="seven">7</Button>
-      <Button id="eight">8</Button>
-      <Button id="nine">9</Button>
-      <Button id="add">+</Button>
-      <Button id="subtract">-</Button>
-      <Button id="multiply">*</Button>
-      <Button id="divide">/</Button>
-      <Button id="decimal">.</Button>
-      <Button id="clear">AC</Button>
-      <Container id="display">Display calc here</Container>
+      <Container
+        id="display"
+        style={{ backgroundColor: "black", color: "white" }}
+      >
+        Display calc here 0
+      </Container>
+      <Container style={{ backgroundColor: "grey", padding: 0 }}>
+        <Row>
+          <Col xs={6} style={{ paddingRight: 0 }}>
+            <Button
+              id="clear"
+              style={{ borderRadius: 0, width: "100%" }}
+              variant="danger"
+            >
+              AC
+            </Button>
+          </Col>
+          <Col style={{ paddingLeft: 0, paddingRight: 0 }}>
+            <Button
+              id="divide"
+              style={{ borderRadius: 0, width: "100%" }}
+              variant="secondary"
+            >
+              /
+            </Button>
+          </Col>
+          <Col style={{ paddingLeft: 0 }}>
+            <Button
+              id="multiply"
+              style={{ borderRadius: 0, width: "100%" }}
+              variant="secondary"
+            >
+              x
+            </Button>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col style={{ paddingRight: 0 }}>
+            <Button
+              id="seven"
+              style={{ borderRadius: 0, width: "100%" }}
+              variant="dark"
+            >
+              7
+            </Button>
+          </Col>
+          <Col style={{ paddingLeft: 0, paddingRight: 0 }}>
+            <Button
+              id="eight"
+              style={{ borderRadius: 0, width: "100%" }}
+              variant="dark"
+            >
+              8
+            </Button>
+          </Col>
+          <Col style={{ paddingLeft: 0, paddingRight: 0 }}>
+            <Button
+              id="nine"
+              style={{ borderRadius: 0, width: "100%" }}
+              variant="dark"
+            >
+              9
+            </Button>
+          </Col>
+          <Col style={{ paddingLeft: 0 }}>
+            <Button
+              id="subtract"
+              style={{ borderRadius: 0, width: "100%" }}
+              variant="secondary"
+            >
+              -
+            </Button>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col style={{ paddingRight: 0 }}>
+            <Button
+              id="four"
+              style={{ borderRadius: 0, width: "100%" }}
+              variant="dark"
+            >
+              4
+            </Button>
+          </Col>
+          <Col style={{ paddingLeft: 0, paddingRight: 0 }}>
+            <Button
+              id="five"
+              style={{ borderRadius: 0, width: "100%" }}
+              variant="dark"
+            >
+              5
+            </Button>
+          </Col>
+          <Col style={{ paddingLeft: 0, paddingRight: 0 }}>
+            <Button
+              id="six"
+              style={{ borderRadius: 0, width: "100%" }}
+              variant="dark"
+            >
+              6
+            </Button>
+          </Col>
+          <Col style={{ paddingLeft: 0 }}>
+            <Button
+              id="add"
+              style={{ borderRadius: 0, width: "100%" }}
+              variant="secondary"
+            >
+              +
+            </Button>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col style={{ paddingRight: 0 }}>
+            <Row>
+              <Col style={{ paddingRight: 0 }}>
+                <Button
+                  id="one"
+                  style={{ borderRadius: 0, width: "100%" }}
+                  variant="dark"
+                >
+                  1
+                </Button>
+              </Col>
+              <Col style={{ paddingLeft: 0, paddingRight: 0 }}>
+                <Button
+                  id="two"
+                  style={{ borderRadius: 0, width: "100%" }}
+                  variant="dark"
+                >
+                  2
+                </Button>
+              </Col>
+              <Col style={{ paddingLeft: 0, paddingRight: 0 }}>
+                <Button
+                  id="three"
+                  style={{ borderRadius: 0, width: "100%" }}
+                  variant="dark"
+                >
+                  3
+                </Button>
+              </Col>
+
+              <Col style={{ paddingRight: 0 }}>
+                <Button
+                  id="zero"
+                  style={{ borderRadius: 0, width: "100%" }}
+                  variant="dark"
+                >
+                  0
+                </Button>
+              </Col>
+              <Col style={{ paddingLeft: 0 }}>
+                <Button
+                  id="decimal"
+                  style={{ borderRadius: 0, width: "100%" }}
+                  variant="dark"
+                >
+                  .
+                </Button>
+              </Col>
+            </Row>
+          </Col>
+
+          <Col style={{ paddingLeft: 0 }}>
+            <Button
+              id="equals"
+              style={{ borderRadius: 0, width: "100%", height: "100%" }}
+            >
+              =
+            </Button>
+          </Col>
+        </Row>
+      </Container>
     </Container>
   );
 }
