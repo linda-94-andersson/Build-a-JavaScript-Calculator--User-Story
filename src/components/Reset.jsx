b7c51fb5825a675f5fda5d471512a7e537717ecc
Linda Andersson <linda.29.andersson@gmail.com>
2022-12-27 11:23:02 +0100

    Added function to reset inputs

--- src/components/Reset.jsx
+++ src/components/Reset.jsx
@@ -1,13 +1,22 @@
 import react from "react";
 import { Button, Col } from "react-bootstrap";
+import { useRecoilState } from "recoil";
+import { displayState } from "../atoms/atom";
 
 const Reset = () => {
+  const [display, setDisplay] = useRecoilState(displayState);
+
+  const handleClick = () => {
+    setDisplay("0");
+  };
+
   return (
     <Col xs={6} style={{ paddingRight: 0 }}>
       <Button
         id="clear"
         style={{ borderRadius: 0, width: "100%" }}
         variant="danger"
+        onClick={handleClick}
       >
         AC
       </Button>
