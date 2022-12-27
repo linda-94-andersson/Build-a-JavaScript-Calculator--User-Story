10ef50a416a9907435b31c5591ddfa87bddfe3ca
Linda Andersson <linda.29.andersson@gmail.com>
2022-12-27 11:48:36 +0100

    Setup for selecting an operator

--- src/components/operatiors/Multiply.jsx
+++ src/components/operatiors/Multiply.jsx
@@ -1,13 +1,22 @@
 import react from "react";
-import { Container, Button, Col, Row } from "react-bootstrap";
+import { Button, Col } from "react-bootstrap";
+import { useRecoilState } from "recoil";
+import { operatorState } from "../../atoms/atom";
 
 const Multiply = () => {
+  const [operator, setOperator] = useRecoilState(operatorState);
+
+  const handleClick = () => {
+    setOperator("*");
+  };
+
   return (
     <Col style={{ paddingLeft: 0 }}>
       <Button
         id="multiply"
         style={{ borderRadius: 0, width: "100%" }}
         variant="secondary"
+        onClick={handleClick}
       >
         x
       </Button>
--- src/components/operatiors/Subtrack.jsx
+++ src/components/operatiors/Subtrack.jsx
@@ -1,13 +1,22 @@
 import react from "react";
 import { Button, Col } from "react-bootstrap";
+import { useRecoilState } from "recoil";
+import { operatorState } from "../../atoms/atom";
 
 const Subtrack = () => {
+  const [operator, setOperator] = useRecoilState(operatorState);
+
+  const handleClick = () => {
+    setOperator("-");
+  };
+
   return (
     <Col style={{ paddingLeft: 0 }}>
       <Button
         id="subtract"
         style={{ borderRadius: 0, width: "100%" }}
         variant="secondary"
+        onClick={handleClick}
       >
         -
       </Button>
