8bcb5a2c261e8dda14bbb44e62f6b87cf9ee1fe1
Linda Andersson <linda.29.andersson@gmail.com>
2022-12-27 11:34:14 +0100

    Added function do add period dot only once per display

--- src/components/operatiors/Decimal.jsx
+++ src/components/operatiors/Decimal.jsx
@@ -1,13 +1,26 @@
 import react from "react";
 import { Button, Col } from "react-bootstrap";
+import { useRecoilState } from "recoil";
+import { displayState } from "../../atoms/atom";
 
 const Decimal = () => {
+  const [display, setDisplay] = useRecoilState(displayState);
+
+  const handleClick = () => {
+    if (display.match(/[\.]/gm)) {
+      return;
+    } else {
+      setDisplay(display + ".");
+    }
+  };
+
   return (
     <Col style={{ paddingLeft: 0, paddingRight: 0 }}>
       <Button
         id="decimal"
         style={{ borderRadius: 0, width: "100%" }}
         variant="dark"
+        onClick={handleClick}
       >
         .
       </Button>
