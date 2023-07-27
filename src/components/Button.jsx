842ecd0ef7d87bb440b183b7965317cc5d3f2616
Linda Andersson <linda.29.andersson@gmail.com>
2023-07-27 10:55:13 +0200

    Added id as prop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Button as BootstrapButton, Col } from "react-bootstrap";
 
-const Button = ({ onClick, children, variant }) => {
+const Button = ({ onClick, children, variant, id }) => {
   return (
-    <Col style={{ paddingLeft: 0, paddingRight: 0 }}>
+    <Col style={{ paddingLeft: 0, paddingRight: 0 }} id={id}>
       <BootstrapButton
         style={{ borderRadius: 0, width: "100%" }}
         variant={variant ? variant : "secondary"}
